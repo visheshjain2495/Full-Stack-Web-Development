bb65a3034fd284980ef1d650067d837382a5d552
visheshjain2495 <visheshjain003@icloud.com>
2025-05-13 18:24:18 +0530

    progress 13th may, 2025

---
+++ Backend/5.3 Axios/index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const port = 3001;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get("https://bored-api.appbrewery.com/random");
+    const result = response.data;
+    res.render("solution.ejs", { data: result });
+  } catch (error) {
+    res.render("solution.ejs", {
+      error: error.message,
+    });
+  }
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://bored-api.appbrewery.com/filter?type=${req.body.type}&participants=${req.body.participants}`
+    );
+    const result = response.data;
+    res.render("solution.ejs", {
+      data: result[Math.floor(Math.random() * result.length)],
+    });
+  } catch (error) {
+    res.render("solution.ejs", {
+      error: "No activities that match your criteria.",
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port: http://localhost:${port}`);
+});
