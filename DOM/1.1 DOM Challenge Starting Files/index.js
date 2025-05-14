3ccc2107ae6de23496474196110fa52fa13f9408
visheshjain2495 <visheshjain003@icloud.com>
2025-05-14 16:51:37 +0530

    Progress 14th May, 2025

---
+++ DOM/1.1 DOM Challenge Starting Files/index.js
@@ -0,0 +1,5 @@
+document.querySelectorAll("ul li")[2].innerHTML = "Vishesh";
+
+document.querySelector("a").style="color : red; text-decoration : none;";
+
+document.querySelector("button").style.backgroundColor = "yellow";
